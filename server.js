5cf1a6c692496ae7c5ba3c657e3b1410907ae713
waqaskhan <mwaqas3030@gmail.com>
2021-03-11 08:50:02 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 1000;
+server.use(middleware);
+server.use(router);
+server.listen(port);
